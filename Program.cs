bdc9d32f95e709d0dc2ac18a73a12bfa3ec13baf
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2022-09-20 10:17:53 -0300

    Executando um teste de debug de uma condicional if

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Executando um debug de condicional
+ 
+int inventoryAmmount = 2;
+int soldAmmount = 3;
+
+bool success = inventoryAmmount >= soldAmmount;
+
+if (success)
+{
+    Console.WriteLine("Venda Realizada");
+}
+else
+{
+    Console.WriteLine("Impossível realizar a venda.");
+}
+
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Repositório de anotações do Bootcamp (Pottencial .NET Developer)
 Obs: Criando seu Primeiro Repositório no GitHub Para Compartilhar Seu Progresso
 
 Desafio de Projeto 1 - 19/09/2022
+
