1bba81c8e827647c686ed74a14dec9dbeae5d0fb
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2022-09-22 08:04:14 -0300

    Criando um menu de opçoes interativo

--- Program.cs
+++ Program.cs
@@ -2,6 +2,60 @@
 using pottencial_developer_2022.Models;
 int inventoryAmmount = 2;
 int soldAmmount = 3;
+bool exit = true;
+char option = '0';
+string display = "";
+
+while (exit)
+{
+    Console.Clear();
+    Console.WriteLine("************************************");
+    Console.WriteLine(display);
+    Console.WriteLine("************************************");
+    Console.WriteLine("Escolha uma opção no menu abaixo:");
+    Console.WriteLine("1 - Cadastrar");
+    Console.WriteLine("2 - Editar");
+    Console.WriteLine("3 - Deletar");
+    Console.WriteLine("4 - Buscar");
+    Console.WriteLine("0 - Sair");
+
+    option = Convert.ToChar(Console.ReadLine());
+    switch (option)
+    {
+        case '1':
+            {
+                display = "Cadastro de clientes.";
+                break;
+            }
+        case '2':
+            {
+                display = "Editar clientes.";
+                break;
+            }
+        case '3':
+            {
+                display = "Deletar clientes.";
+                break;
+            }
+        case '4':
+            {
+                display = "Buscar clientes.";
+                break;
+            }
+        case '0':
+            {
+                Console.WriteLine("Programa encerrado.");
+                exit = false;
+                break;
+            }
+        default:
+            {
+
+                display = "Opção inválida.";
+                break;
+            }
+    }
+}
 
 bool success = inventoryAmmount >= soldAmmount;
 
