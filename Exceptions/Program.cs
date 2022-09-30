e0e5c78624a655193bfab6e3dd92e7882c88f38a
Ivanildo Cândido Bezerra <systechtecnologia@gmail.com>
2022-09-30 07:25:50 -0300

    Exemplos de fila, pilha e dicionário

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,7 +1,34 @@
 ﻿
 string[] lines = File.ReadAllLines("Files/fileModel.txt");
 
+Queue<string> queue = new Queue<string>();
+Stack<string> stack = new Stack<string>();
+Dictionary<string, string> states = new Dictionary<string, string>();
+
 foreach (string line in lines)
 {
-    Console.WriteLine(line);
+    queue.Enqueue(line);
+    stack.Push(line);
+}
+Console.WriteLine("Implementação de um fila:");
+for (int i = 0; i < lines.Length; i++)
+{
+
+    Console.WriteLine($"{i + 1}° da fila: {queue.Dequeue()}");
+}
+
+Console.WriteLine("Implementação de um pilha:");
+for (int i = 0; i < lines.Length; i++)
+{
+
+    Console.WriteLine($"{i + 1}° da pilha: {stack.Pop()}");
+}
+
+states["PB"] = "Paraíba";
+states["RJ"] = "Rio de Janeiro";
+states["SP"] = "São Paulo";
+
+foreach (var state in states)
+{
+    Console.WriteLine($"Chave = {state.Key} - Valor = {state.Value}");
 }
